aced1eee36fd1d3cf35bf6e33ffda3ac6cc0adee
Kiran <kiran.sadara6730@gmail.com>
2026-01-28 23:07:53 +0530

    prisma added successfully

--- database.js
+++ database.js
@@ -1,6 +1,16 @@
+import { PrismaClient } from "./generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+import "dotenv/config";
 
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export default prisma;
